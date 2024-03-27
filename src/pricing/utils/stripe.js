53702cd21d39e9cd14422a14e305a30ef270e651
Miracle Billy <uniquemiraculous@gmail.com>
2024-03-27 15:08:29 +0100

    final update

--- src/pricing/utils/stripe.js
+++ src/pricing/utils/stripe.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe("sk_test_394X95sofRPtG0dylz9pm94j00YnmT990H", {
-    apiVersion: "2022-11-15",
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
 });
